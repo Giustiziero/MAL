56980adb8c5d63c506ed52e64165670dc0941a01
Giustiziero <gtissot6@gatech.edu>
2024-06-23 14:43:44 -0700

    fixed multiple api calls for search bar

--- mal_frontend/src/pages/ItemRecPage/AutoSearchBar.js
+++ mal_frontend/src/pages/ItemRecPage/AutoSearchBar.js
@@ -7,25 +7,32 @@ import '../../App.css';  // Ensure your CSS styles are imported
 const AutocompleteSearchBar = ({ onSearch }) => {
   const [value, setValue] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [allSuggestions, setAllSuggestions] = useState([]);
+
+  useEffect(() => {
+    const fetchSuggestions = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:5000/api/suggestions');
+        setAllSuggestions(response.data);
+        console.log('Fetched suggestions:', response.data);
+      } catch (error) {
+        console.error('Error fetching suggestions:', error);
+      }
+    };
+
+    fetchSuggestions();
+  }, []);
 
   useEffect(() => {
     if (value.trim() !== '') {
-      const fetchSuggestions = async () => {
-        try {
-          const response = await axios.get('http://127.0.0.1:5000/api/suggestions', {
-            params: { query: value },
-          });
-          setSuggestions(response.data);
-        } catch (error) {
-          console.error('Error fetching suggestions:', error);
-        }
-      };
-
-      fetchSuggestions();
+      const filteredSuggestions = allSuggestions.filter(suggestion =>
+        suggestion.toLowerCase().includes(value.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
     } else {
       setSuggestions([]);
     }
-  }, [value]);
+  }, [value, allSuggestions]);
 
   const handleInputChange = (event) => {
     setValue(event.target.value);
