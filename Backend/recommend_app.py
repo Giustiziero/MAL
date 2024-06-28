7f39fafdfe2ad163d27d536014bbe7969a70f3e0
Giustiziero <gtissot6@gatech.edu>
2024-06-27 17:55:36 -0700

    Connected Database to Application fully

--- Backend/recommend_app.py
+++ Backend/recommend_app.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 from flask_cors import CORS
-from query_sim_mat import get_similar_animes
+from query_sim_mat import get_similar_animes, get_index_dict
 from search_bar_recs import get_suggestions  # Import the new function from database module
 
 app = Flask(__name__)
 CORS(app)
 
 # Load your similarity DataFrame (assuming it's stored as a CSV file locally)
-similarity_df = pd.read_csv('./Temp_data/cosine_sim_mat.csv', index_col=0)
+# similarity_df = pd.read_csv('./Temp_data/cosine_sim_mat.csv', index_col=0)
+index_dict = get_index_dict()
 
 @app.route('/get_similar_animes', methods=['GET'])
 def get_similar_animes_endpoint():
@@ -19,7 +20,7 @@ def get_similar_animes_endpoint():
         return jsonify({'error': 'Anime name is required'}), 400
 
     try:
-        similar_animes = get_similar_animes(anime_name, similarity_df, top_n)
+        similar_animes = get_similar_animes(anime_name, index_dict, top_n)
         # Convert the Series to a list of tuples
         similar_animes_list = list(similar_animes.items())
         return jsonify(similar_animes_list)
@@ -30,9 +31,9 @@ def get_similar_animes_endpoint():
 def get_suggestions_endpoint():
     query = request.args.get('query', '').lower()
     if not query:
-        return jsonify(list(similarity_df.columns))
+        return jsonify(list(index_dict.keys()))
 
-    suggestions = get_suggestions(query, similarity_df)
+    suggestions = get_suggestions(query, index_dict)
     return jsonify(suggestions)
 
 if __name__ == '__main__':
--- Backend/search_bar_recs.py
+++ Backend/search_bar_recs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-def get_suggestions(query, similarity_df):
+def get_suggestions(query, index_dict):
+    anime_list = list(index_dict.keys())
     # Filter the anime titles based on the query
-    suggestions = [title for title in similarity_df.columns if query in title.lower()]
+    suggestions = [title for title in anime_list if query in title.lower()]
     return suggestions
