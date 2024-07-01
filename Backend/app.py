a1b98c293739ad191c326212fdd555b084c4691a
Giustiziero <gtissot6@gatech.edu>
2024-06-30 18:10:53 -0700

    added logging to main app

--- Backend/app.py
+++ Backend/app.py
@@ -3,16 +3,22 @@ import pandas as pd
 from flask_cors import CORS
 from query_sim_mat import get_similar_animes, get_index_dict
 from search_bar_recs import get_suggestions  # Import the new function from database module
+import logging 
 
 app = Flask(__name__)
 CORS(app)
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+logging.info("file just ran")
 # Load your similarity DataFrame (assuming it's stored as a CSV file locally)
 # similarity_df = pd.read_csv('./Temp_data/cosine_sim_mat.csv', index_col=0)
 index_dict = get_index_dict()
 
 @app.route('/get_similar_animes', methods=['GET'])
 def get_similar_animes_endpoint():
+    logging.info("get similar anime received")
     anime_name = request.args.get('anime_name')
     top_n = request.args.get('top_n', default=25, type=int)
 
@@ -29,6 +35,7 @@ def get_similar_animes_endpoint():
 
 @app.route('/api/suggestions', methods=['GET'])
 def get_suggestions_endpoint():
+    logging.info("get suggestions received")
     query = request.args.get('query', '').lower()
     if not query:
         return jsonify(list(index_dict.keys()))
