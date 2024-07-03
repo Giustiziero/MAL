977ae9d0b151b21cdf95ff0761572d56384ec9c2
Giustiziero <gtissot6@gatech.edu>
2024-07-03 11:21:07 -0700

    Uncommented old functions

--- Backend/app.py
+++ Backend/app.py
@@ -28,32 +28,32 @@ def index():
    logger.info('Request for index page received')
    return render_template('index.html')
 
-# @app.route('/get_similar_animes', methods=['GET'])
-# def get_similar_animes_endpoint():
-#     logger.info("get similar anime received")
-#     anime_name = request.args.get('anime_name')
-#     top_n = request.args.get('top_n', default=25, type=int)
+@app.route('/get_similar_animes', methods=['GET'])
+def get_similar_animes_endpoint():
+    logger.info("get similar anime received")
+    anime_name = request.args.get('anime_name')
+    top_n = request.args.get('top_n', default=25, type=int)
 
-#     if not anime_name:
-#         return jsonify({'error': 'Anime name is required'}), 400
+    if not anime_name:
+        return jsonify({'error': 'Anime name is required'}), 400
 
-#     try:
-#         similar_animes = get_similar_animes(anime_name, index_dict, top_n)
-#         # Convert the Series to a list of tuples
-#         similar_animes_list = list(similar_animes.items())
-#         return jsonify(similar_animes_list)
-#     except KeyError:
-#         return jsonify({'error': 'Anime not found in the similarity matrix'}), 404
+    try:
+        similar_animes = get_similar_animes(anime_name, index_dict, top_n)
+        # Convert the Series to a list of tuples
+        similar_animes_list = list(similar_animes.items())
+        return jsonify(similar_animes_list)
+    except KeyError:
+        return jsonify({'error': 'Anime not found in the similarity matrix'}), 404
 
-# @app.route('/api/suggestions', methods=['GET'])
-# def get_suggestions_endpoint():
-#     logger.info("get suggestions received")
-#     query = request.args.get('query', '').lower()
-#     if not query:
-#         return jsonify(list(index_dict.keys()))
+@app.route('/api/suggestions', methods=['GET'])
+def get_suggestions_endpoint():
+    logger.info("get suggestions received")
+    query = request.args.get('query', '').lower()
+    if not query:
+        return jsonify(list(index_dict.keys()))
 
-#     suggestions = get_suggestions(query, index_dict)
-#     return jsonify(suggestions)
+    suggestions = get_suggestions(query, index_dict)
+    return jsonify(suggestions)
 
 if __name__ == '__main__': 
     app.run()
